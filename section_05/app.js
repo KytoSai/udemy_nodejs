4b4fe342d0a779cefc475c8177f9f8f9ed8182ff
kytosai <kytosai@gmail.com>
2019-12-19 00:04:31 +0700

    Finsih 71. Returning a 404 Page

--- section_05/app.js
+++ section_05/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -17,9 +18,7 @@ app.use(shopRoutes);
 app.use('/',(req, res, next) => {
   res
     .status(404) // quan trọng cần phải set status 404 cho page này
-    .send(`
-      <h1>Page not found</h1>
-    `)
+    .sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 
