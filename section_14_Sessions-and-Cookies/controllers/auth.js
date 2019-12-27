d1103b53a7fd6f5e2c4a13f05e46bd4ab08cc5d5
kytosai <kytosai@gmail.com>
2019-12-28 00:42:51 +0700

    Finish 239. Using the Session Middleware

--- section_14_Sessions-and-Cookies/controllers/auth.js
+++ section_14_Sessions-and-Cookies/controllers/auth.js
@@ -1,16 +1,5 @@
 exports.getLogin = (req, res, next) => {
-  // Lấy giá trị của cookie `loggedIn` đã gán trước đó
-  console.log('[Cookie]', req.get("Cookie"))
-  let isLoggedIn = false;
-  if(
-    req.get("Cookie") &&
-    req.get("Cookie").split(";")[0].trim() &&
-    req.get("Cookie").split(";")[0].trim().split("=")[1] === 'true'
-  ) {
-    isLoggedIn = true;
-  }
-
-  console.log(isLoggedIn);
+  console.log(req.session);
 
   res.render("auth/login", {
     path: "/login",
@@ -20,6 +9,6 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  res.setHeader("Set-Cookie", "loggedIn=true");
+  req.session.isLoggedIn = true;
   res.redirect("/");
 };
