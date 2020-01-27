eee2ff06be572d804d4e5cce9da4cc355808ffaf
kytosai <kytosai@gmail.com>
2020-01-27 09:39:41 +0700

    Finish 375. Setting Up a Post Model

--- section_25_working-with-rest-apis-the-practical-app/README.md
+++ section_25_working-with-rest-apis-the-practical-app/README.md
@@ -1,5 +1,9 @@
 # COURSE
 
+## CONFIG MONGODB
+
+- Url config `mongodb+srv://user01:123123123@cluster0-1cukr.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true`
+
 ## SECTION 25 - Working with REST APIs - The Practical Application
 
 ### 369. REST APIs & The Rest Of The Course 
@@ -21,5 +25,8 @@
 - (chưa làm theo video)
 - Sử dụng express-validator để validate input của api 
 
+### 375. Setting Up a Post Model
 
+- Trong cấu hình `schema` của model post, ta thêm option `{ timestamps: true }` nó giúp cho tự động tạo `createdAt` và `updatedAt` mỗi khi có sự thay đổi data
+  - Quote `4:06` - `when a new version is added to the database, when a new object is added to the database. So we automatically get a createdAt and updatedAt timestamp out of the box then` 
 
--- section_25_working-with-rest-apis-the-practical-app/backend/app.js
+++ section_25_working-with-rest-apis-the-practical-app/backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
@@ -9,12 +10,22 @@ const app = express();
 app.use(bodyParser.json()); // application/json
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+mongoose
+  .connect(
+    'mongodb+srv://user01:123123123@cluster0-1cukr.mongodb.net/udemy_nodejs_section25?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true'
+  )
+  .then(result => {
+    app.listen(8080);
+  })
+  .catch(err => console.log(err));
